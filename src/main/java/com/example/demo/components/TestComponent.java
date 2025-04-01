4d30493e9eb417ba653a528f37bf66900621fb93
DongSeong Seo <sds901234@naver.com>
2025-04-01 11:02:11 +0900

    third commit

---
+++ src/main/java/com/example/demo/components/TestComponent.java
@@ -0,0 +1,12 @@
+package com.example.demo.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestComponent {
+
+    public TestComponent() {
+        System.out.println("Test Component");
+    }
+
+}
